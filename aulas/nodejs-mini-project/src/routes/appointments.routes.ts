0aa9f068caf82727202cdf56e478d2131adb5418
Julian Kuroiwa <julian.kuroiwa@gmail.com>
2020-04-13 14:35:12 -0300

    date validation

--- aulas/nodejs-mini-project/src/routes/appointments.routes.ts
+++ aulas/nodejs-mini-project/src/routes/appointments.routes.ts
@@ -1,20 +1,39 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { parseISO, startOfHour, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
 
-const apppointments = [];
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
+
+const appointments: Appointment[] = [];
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date)
+  );
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'This appointment is already booked' });
+  }
+
   const appointment = {
     id: uuid(),
     provider,
-    date,
+    date: parsedDate,
   };
 
-  apppointments.push(appointment);
+  appointments.push(appointment);
 
   return response.json(appointment);
 });
